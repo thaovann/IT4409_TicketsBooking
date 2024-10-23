60747cfb2dc35d4a4f5f3a1451fd61089cfa55cd
freeGuy2908 <antmanyb123@gmail.com>
2024-10-23 10:02:47 +0700

    login ver 1

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-require("dotenv").config(); 
+const cors = require("cors")
+require("dotenv").config();
 
 const app = express();
 app.use(bodyParser.json());
@@ -8,14 +9,16 @@ app.use(bodyParser.json());
 //middleware
 app.use(express.json());
 
+app.use(cors());
+
 const authRoutes = require("./routes/authRoutes");
 app.use("/auth", authRoutes);
 
-const eventRoutes = require("./routes/eventRoutes"); 
+const eventRoutes = require("./routes/eventRoutes");
 app.use("/api/events", eventRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
@@ -25,7 +28,7 @@ const mongoose = require("mongoose");
 const queryString = process.env.MONGODB_URI;
 
 //configure mongoose
-mongoose.connect( queryString, { 
+mongoose.connect(queryString, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => console.log("Database connected successfully!"));
