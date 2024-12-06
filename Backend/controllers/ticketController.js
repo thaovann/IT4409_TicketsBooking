5d07a8b935c6aa10ce1af5a9b17b8a949ee3175f
thaovann <lethithaovan2711@gmail.com>
2024-12-06 13:59:46 +0700

    add API get ticket

--- Backend/controllers/ticketController.js
+++ Backend/controllers/ticketController.js
@@ -205,3 +205,78 @@ exports.deleteTicketCategory = [
         }
     },
 ];
+
+exports.getAllTicketsByCategory = [
+  async (req, res) => {
+    try {
+      const { ticketCategoryId } = req.params;
+      if (!ticketCategoryId) {
+        return res.status(400).json({ message: "Thiếu ticketCategoryId" });
+      }
+
+      const ticketCategory = await TicketCategory.findById(ticketCategoryId);
+      if (!ticketCategory) {
+        return res
+          .status(404)
+          .json({ message: "Không tìm thấy Ticket Category" });
+      }
+
+      const tickets = await Ticket.find({ categoryId: ticketCategoryId });
+
+      return res.status(200).json({
+        message: "Danh sách tickets được lấy thành công",
+        tickets,
+      });
+    } catch (error) {
+      console.error("Lỗi khi lấy danh sách tickets:", error);
+      res.status(500).json({
+        message: "Lỗi khi lấy danh sách tickets",
+        error: error.message,
+      });
+    }
+  },
+];
+
+exports.getAvailableTicket = [
+  async (req, res) => {
+    try {
+      const { ticketCategoryId } = req.params;
+
+      if (!ticketCategoryId) {
+        return res.status(400).json({ message: "Thiếu ticketCategoryId" });
+      }
+
+      const ticketCategory = await TicketCategory.findById(ticketCategoryId);
+      if (!ticketCategory) {
+        return res
+          .status(404)
+          .json({ message: "Không tìm thấy Ticket Category" });
+      }
+
+      const availableTickets = await Ticket.find({
+        categoryId: ticketCategoryId,
+        state: "available",
+      });
+      if (availableTickets.length === 0) {
+        return res
+          .status(404)
+          .json({ message: "Không còn ticket nào có trạng thái 'available'" });
+      }
+
+
+      const randomIndex = Math.floor(Math.random() * availableTickets.length);
+      const randomTicket = availableTickets[randomIndex];
+
+      return res.status(200).json({
+        message: "Ticket được lấy thành công",
+        ticket: randomTicket,
+      });
+    } catch (error) {
+      console.error("Lỗi khi lấy ticket:", error);
+      res.status(500).json({
+        message: "Lỗi khi lấy ticket",
+        error: error.message,
+      });
+    }
+  },
+];
