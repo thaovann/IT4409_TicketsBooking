02013806a2f878a6a6d7372a83a2dc1c4105dfc9
ngoctung1707 <ngoctung17072002dia@gmail.com>
2024-12-13 00:41:17 +0700

    hoàn thiện trang HomePage

--- Frontend/src/pages/User/HomePage.jsx
+++ Frontend/src/pages/User/HomePage.jsx
@@ -7,6 +7,10 @@ import Footer from "../../components/common/Footer";
 import EventCard from "../../components/events/EventCard";
 import "./HomePage.css";
 import voucherImg from "../../assets/img/voucher-img.webp";
+import hochiminh from "../../assets/img/hochiminh.webp";
+import hanoi from "../../assets/img/hanoi.webp";
+import dalat from "../../assets/img/dalat.webp";
+import vitrikhac from "../../assets/img/vitrikhac.webp";
 
 function HomePage() {
   const user = useSelector((state) => state.auth.login.currentUser);
@@ -16,6 +20,9 @@ function HomePage() {
   const [currentBannerIndex, setCurrentBannerIndex] = useState(0);
   // State cho EventCard slider
   const [currentCardIndex, setCurrentCardIndex] = useState(0);
+  const [currentCardIndexMusic, setCurrentCardIndexMusic] = useState(0);
+  const [currentCardIndexArt, setCurrentCardIndexArt] = useState(0);
+  const [currentCardIndexOther, setCurrentCardIndexOther] = useState(0);
 
   useEffect(() => {
     const fetchEvents = async () => {
@@ -25,8 +32,7 @@ function HomePage() {
         );
         const approvedEvents = response.data
           .filter((event) => event.state === "approved")
-          .sort((a, b) => new Date(b.startTime) - new Date(a.startTime))
-          .slice(0, 12);
+          .sort((a, b) => new Date(b.startTime) - new Date(a.startTime));
 
         const eventsWithPrices = await Promise.all(
           approvedEvents.map(async (event) => {
@@ -61,9 +67,7 @@ function HomePage() {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentBannerIndex(
-        (prevIndex) => (prevIndex + 1) % Math.ceil(events.length / 2)
-      );
+      setCurrentBannerIndex((prevIndex) => (prevIndex + 1) % Math.ceil(3));
     }, 5000);
 
     return () => clearInterval(interval);
@@ -98,6 +102,41 @@ function HomePage() {
       prevIndex + 1 >= events.length - 3 ? 0 : prevIndex + 1
     );
   };
+  const prevCardSlideMusic = () => {
+    setCurrentCardIndexMusic((prevIndex) =>
+      prevIndex === 0 ? events.length - 4 : Math.max(0, prevIndex - 1)
+    );
+  };
+
+  const nextCardSlideMusic = () => {
+    setCurrentCardIndexMusic((prevIndex) =>
+      prevIndex + 1 >= events.length - 3 ? 0 : prevIndex + 1
+    );
+  };
+
+  const prevCardSlideArt = () => {
+    setCurrentCardIndexArt((prevIndex) =>
+      prevIndex === 0 ? events.length - 4 : Math.max(0, prevIndex - 1)
+    );
+  };
+
+  const nextCardSlideArt = () => {
+    setCurrentCardIndexArt((prevIndex) =>
+      prevIndex + 1 >= events.length - 3 ? 0 : prevIndex + 1
+    );
+  };
+
+  const prevCardSlideOther = () => {
+    setCurrentCardIndexOther((prevIndex) =>
+      prevIndex === 0 ? events.length - 4 : Math.max(0, prevIndex - 1)
+    );
+  };
+
+  const nextCardSlideOther = () => {
+    setCurrentCardIndexOther((prevIndex) =>
+      prevIndex + 1 >= events.length - 3 ? 0 : prevIndex + 1
+    );
+  };
 
   const handleLoginClick = () => {
     navigate("/login");
@@ -117,7 +156,7 @@ function HomePage() {
                   transform: `translateX(-${currentBannerIndex * 100}%)`,
                 }}
               >
-                {events.map((event, index) => (
+                {events.slice(0, 6).map((event, index) => (
                   <Link
                     to={`/events/${event._id}`}
                     className="banner-slide"
@@ -161,17 +200,15 @@ function HomePage() {
 
               {/* Dots điều hướng */}
               <div className="banner-dots">
-                {Array.from({ length: Math.ceil(events.length / 2) }).map(
-                  (_, index) => (
-                    <div
-                      key={index}
-                      className={`banner-dot ${
-                        index === currentBannerIndex ? "active" : ""
-                      }`}
-                      onClick={() => handleDotClick(index)}
-                    ></div>
-                  )
-                )}
+                {Array.from({ length: Math.ceil(3) }).map((_, index) => (
+                  <div
+                    key={index}
+                    className={`banner-dot ${
+                      index === currentBannerIndex ? "active" : ""
+                    }`}
+                    onClick={() => handleDotClick(index)}
+                  ></div>
+                ))}
               </div>
             </div>
           )}
@@ -213,8 +250,161 @@ function HomePage() {
           <img src={voucherImg} alt="voucher" className="voucher-img" />
         </section>
         {/* -----------------------Nhạc sống------------------------- */}
-        <section className="search1">
-          <h2 className="search-events-title">Nhạc sống</h2>
+        <section className="music-events">
+          <div className="music-events-header">
+            <h2 className="music-events-title">Nhạc sống</h2>
+            <Link to={`/search?query=Nhạc%20sống`} className="see-more">
+              Xem thêm ❯
+            </Link>
+          </div>
+          <div
+            className="event-list"
+            style={{
+              transform: `translateX(-${currentCardIndexMusic * (100 / 4)}%)`,
+            }}
+          >
+            {events.length > 0 ? (
+              events
+                .filter(
+                  (event) =>
+                    event.state === "approved" && event.tags.includes("music")
+                )
+                // .slice(currentCardIndex, currentCardIndex + 4)
+                .map((event) => <EventCard key={event.id} event={event} />)
+            ) : (
+              <p>Không có sự kiện nào.</p>
+            )}
+          </div>
+          {/* Nút điều hướng slider */}
+          <button
+            className="slider-arrow slider-arrow-left"
+            onClick={prevCardSlideMusic}
+          >
+            ❮
+          </button>
+          <button
+            className="slider-arrow slider-arrow-right"
+            onClick={nextCardSlideMusic}
+          >
+            ❯
+          </button>
+        </section>
+        {/* -----------------------Sân khấu và nghệ thuật------------------------- */}
+        <section className="art-events">
+          <div className="art-events-header">
+            <h2 className="art-events-title">Sân khấu & Nghệ thuật</h2>
+            <Link
+              to={`/search/search?query=Sân%20khấu%20&%20Nghệ%20thuật`}
+              className="see-more"
+            >
+              Xem thêm ❯
+            </Link>
+          </div>
+          <div
+            className="event-list"
+            style={{
+              transform: `translateX(-${currentCardIndexArt * (100 / 4)}%)`,
+            }}
+          >
+            {events.length > 0 ? (
+              events
+                .filter(
+                  (event) =>
+                    event.state === "approved" &&
+                    // event.tags.includes("art", "stage")
+                    event.tags.includes("music")
+                )
+                // .slice(currentCardIndex, currentCardIndex + 4)
+                .map((event) => <EventCard key={event.id} event={event} />)
+            ) : (
+              <p>Không có sự kiện nào.</p>
+            )}
+          </div>
+          {/* Nút điều hướng slider */}
+          <button
+            className="slider-arrow slider-arrow-left"
+            onClick={prevCardSlideArt}
+          >
+            ❮
+          </button>
+          <button
+            className="slider-arrow slider-arrow-right"
+            onClick={nextCardSlideArt}
+          >
+            ❯
+          </button>
+        </section>
+        {/* -----------------------Thể loại khác------------------------- */}
+        <section className="others-events">
+          <div className="others-events-header">
+            <h2 className="others-events-title">Thể loại khác</h2>
+            <Link to={`/search?query=Khác`} className="see-more">
+              Xem thêm ❯
+            </Link>
+          </div>
+          <div
+            className="event-list"
+            style={{
+              transform: `translateX(-${currentCardIndexOther * (100 / 4)}%)`,
+            }}
+          >
+            {events.length > 0 ? (
+              events
+                .filter(
+                  (event) =>
+                    event.state === "approved" &&
+                    // event.tags.includes("art", "stage")
+                    event.tags.includes("music")
+                )
+                // .slice(currentCardIndex, currentCardIndex + 4)
+                .map((event) => <EventCard key={event.id} event={event} />)
+            ) : (
+              <p>Không có sự kiện nào.</p>
+            )}
+          </div>
+          {/* Nút điều hướng slider */}
+          <button
+            className="slider-arrow slider-arrow-left"
+            onClick={prevCardSlideOther}
+          >
+            ❮
+          </button>
+          <button
+            className="slider-arrow slider-arrow-right"
+            onClick={nextCardSlideOther}
+          >
+            ❯
+          </button>
+        </section>
+        {/* ------------------------Địa điểm đến thú vị-------------------- */}
+        <section className="interest-location">
+          <h2 className="interest-location-title">Điểm đến thú vị</h2>
+          <div className="location-list">
+            <Link
+              to={`/search?location=TP+HCM`}
+              className="location-item hochiminh-img"
+            >
+              <img src={hochiminh} alt="Tp. Hồ Chí Minh" className="img " />
+            </Link>
+            <Link
+              to={`/search?location=Hà+Nội`}
+              className="location-item hanoi-img"
+            >
+              <img src={hanoi} alt="Hà Nội" className="img " />
+            </Link>
+            <Link
+              to={`/search?location=Đà+Lạt`}
+              className="location-item dalat-img"
+            >
+              <img src={dalat} alt="Đà Lạt" className="img " />
+            </Link>
+            <Link
+              to={`/search?location=khác`}
+              className="location-item vitrikhac-img"
+            >
+              <img src={vitrikhac} alt="vị trí khác" className="img " />
+            </Link>
+          </div>
         </section>
       </div>
       <Footer />
